6d49600ce609dec7cee6123a7e30d1741ed860f9
Sehan Shetty <59560852+yttehs123@users.noreply.github.com>
2020-06-30 16:21:58 +0200

    Adding pathfinding code

---
+++ PathFinding/PathMode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Mode
+{
+    BFS = 0,
+    Dijkstra = 1,
+    GreedyBFS = 2,
+    Astar = 3
+}
+public class PathMode : MonoBehaviour
+//Set default algorithm to BFS, which can be changed by selection
+{
+    public Mode mode = Mode.BFS;
+
+    public Mode ReturnMode()
+    {
+        return mode;
+    }
+
+}
